10572e83939c0872cb1fc26075bb630d1ecbd71a
karol <karol.murray@sonalake.com>
2016-11-12 12:38:49 +0000

    Added merging of test data to script

--- run_analysis.R
+++ run_analysis.R
@@ -28,11 +28,8 @@ colnames(x_train) <- features$V2
 ## merging by row index
 ## This will add 2 new columns - Row.names 
 x_tr_sub <- merge(subject_train, x_train, by.x = 0, by.y = 0, sort = FALSE)
-y_tr_sub <- merge(subject_train, y_train, by.x = 0, by.y = 0, sort = FALSE)
-
 
 ## Rename V1 row to subject
-colnames(y_tr_sub)[colnames(y_tr_sub) == "V1"] <- "subject"
 colnames(x_tr_sub)[colnames(x_tr_sub) == "V1"] <- "subject"
 
 ## This will speed things up for the merging
@@ -41,8 +38,43 @@ y_tr_sub <- tbl_df(y_tr_sub)
 
 ## Removing the Row.names column from the x_tr_sub datasets 
 x_tr_sub <- select(x_tr_sub, 2:length(names(x_tr_sub)))
-## Only need the subject and activity columns
+## Only need the activity columns
 y_tr_sub <- select(y_tr_sub,  activity)
-## x_y_tr_merged <- merge(x_tr_sub, y_tr_sub, by.x = "subject", by.y = "subject", sort = FALSE)
 x_y_tr_merged <- cbind(x_tr_sub, y_tr_sub) ## That's the training data merged
+
+## Now the test data
+## Starting with the X_test.txt
+x_test <- read.table("data/test/X_test.txt", stringsAsFactors = FALSE, header = FALSE)
+## Add column names to x_test dataset
+colnames(x_test) <- features$V2
+## Get the subject data
+subject_test <- read.table("data/test/subject_test.txt", stringsAsFactors = FALSE, header = FALSE)
+## Merge subject data with the x_test data
+x_test_sub <- merge(subject_test, x_test, by.x = 0, by.y = 0, sort = FALSE)
+## Rename the V1 col to "subject"
+colnames(x_test_sub)[colnames(x_test_sub) == "V1"] <- "subject"
+## Remove column "Row.names" from the merged x_test_sub dataset
+x_test_sub <- select(x_test_sub, 2:length(names(x_test_sub)))
+## -- finished transforming x_test data
+
+## Now read and transform the the y_test data
+y_test <- read.table("data/test/y_test.txt", stringsAsFactors = FALSE, header = FALSE)
+## Apply the Activty labels to the y_test data
+y_test <- merge(y_test, act_lab, by.x = "V1", by.y = "V1", sort = FALSE)
+colnames(y_test) <- c("class", "activity")
+y_test_sub <- merge(subject_test, y_test, by.x = 0, by.y = 0, sort = FALSE)
+colnames(y_test_sub)[colnames(y_test_sub) == "V1"] <- "subject"
+
+## Only unterested in the activity column
+y_test_sub <- select(y_test_sub, subject, activity)
+
+test_data <- merge(y_test_sub, x_test_sub, by.x = 0, by.y = 0, sort = FALSE) 
+
+## --- Merging of test data complete
+
+
+
+
+
+
 
